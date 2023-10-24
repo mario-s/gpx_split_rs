335c5843af4b34281a693d1d75c492b4e7fc0c45
Mario <ma.schroed73@gmail.com>
2023-10-24 18:39:18 +0200

    add integration test

--- tests/split_it.rs
+++ tests/split_it.rs
@@ -1,11 +1,24 @@
-use split::Context;
-use split::LengthSplitter;
+extern crate gpx_split;
+
+use gpx_split::Context;
+use gpx_split::LengthSplitter;
+use gpx_split::PointsSplitter;
 
 #[test]
-fn test_lengthsplitter() {
+fn test_length() {
     let s = LengthSplitter::new(200);
     let mut c = Context {
-        file: "",
+        file: "tests/test.gpx",
+        strategy: s,
+    };
+    c.execute();
+}
+
+#[test]
+fn test_points() {
+    let s = PointsSplitter::new(500);
+    let mut c = Context {
+        file: "tests/test.gpx",
         strategy: s,
     };
     c.execute();
