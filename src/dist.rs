abb4f724d2734d7ab2d608694ccf4b1c329419fd
Mario <ma.schroed73@gmail.com>
2023-11-14 15:32:45 +0100

    put test at end

--- src/dist.rs
+++ src/dist.rs
@@ -11,17 +11,17 @@ fn distance(points: Vec<Point>) -> f64 {
     distance_vec(points, Unit::Meters)
 }
 
+/// Calculates the distance of all waypoints in the track.
+/// Returns result in Meter.
+pub fn distance_points(way_points: Vec<Waypoint>) -> f64 {
+    let points = way_points.iter().map(|p| p.point()).map(|p| Point::new(p.x(), p.y())).collect();
+    distance(points)
+}
+
 #[test]
 fn test_distance() {
     let point_0 = Point::new(40.7767644, -73.9761399);
     let point_1 = Point::new(40.771209, -73.9673991);
     let distance = distance(vec![point_0, point_1]);
     assert!(distance == 960.9072987659282);
-}
-
-/// Calculates the distance of all waypoints in the track.
-/// Returns result in Meter.
-pub fn distance_points(way_points: Vec<Waypoint>) -> f64 {
-    let points = way_points.iter().map(|p| p.point()).map(|p| Point::new(p.x(), p.y())).collect();
-    distance(points)
 }
