5515437673286142bdb7c9560024efee7ac42a25
Mario <ma.schroed73@gmail.com>
2024-04-27 18:17:47 +0200

    include README to generate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,4 @@
-//!
-//! This tool can split a route or track in a GPX file into smaller chunks.
-//! It will create new files, where each one has a suffix to the filename like so FILENAME_1.gpx.
-//!
-//! <hr/>
-//!
-//! <p>The type of splitting is controlled with command line arguments. Here are some samples to cut a track or route.
-//! <ul>
-//! <li>split a track after 500 points (default)<br/>
-//! <code>gpx_split My_Track.gpx</code></li>
-//! <li>split a route after 600 points<br/>
-//! <code>gpx_split -t route My_Route.gpx -m 600</code></li>
-//! <li>split a track after 50 km (unit is meter)<br/>
-//! <code>gpx_split My_Track.gpx -b len -m 50000</code></li>
-//! <li>split a route after 50 km<br/>
-//! <code>gpx_split -t route My_Route.gpx -b len -m 50000</code></li>
-//! <li>split a track at some locations with a maximum distance to a POI of 20 m (if argument n is not provided, the program will look for splitting points in the source file)<br/>
-//! <code>gpx_split My_Track.gpx -b loc -m 20 -n POI.gpx</code></li>
-//! <li>split a route at some locations with a maximum distance to a POI of 10 m<br/>
-//! <code>gpx_split -t route My_Route.gpx -b loc -m 10 -n POI.gpx</code></li>
-//! </ul>
-//! </p>
+#![doc = include_str!("../README.md")]
 
 mod geo;
 /// Input and output functions.
